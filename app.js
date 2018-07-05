70b679bf1e7bbe74d2f39e7b4fd38b4529df817f
yazeedb <ybzadough@gmail.com>
2018-07-05 19:24:44 -0400

    remove commented code

--- app.js
+++ app.js
@@ -21,12 +21,6 @@ const meter = $('#meter');
 
 const begin = fromEvent(document, 'keydown');
 const end = fromEvent(document, 'keyup');
-// let powerLevel = 0;
-
-// const powerLevels = {
-//   base: 100,
-//   ssj: 5000
-// };
 
 const powerLevels = {
   100: {
@@ -58,22 +52,4 @@ begin.pipe(
 
 end.subscribe(() => {
   sprite.classList.remove('powerup');
-})
-
-//
-// begin
-//   .subscribe(() => {
-//     sprite.classList.add('powerup');
-//
-//     powerLevel += powerLimits.base > powerLevel ? 1 : 0;
-//
-//     const containerWidth = meterContainer.offsetWidth;
-//     const newWidth = (powerLevel / powerLimits.base) * containerWidth;
-//
-//     meter.style.width = `${newWidth}px`;
-//   });
-//
-// end
-//   .subscribe(() => {
-//     sprite.classList.remove('powerup');
-//   });
+});
