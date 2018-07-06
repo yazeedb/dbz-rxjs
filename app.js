9e3b72338a36c9f3af9c407c9e94008259e76b76
yazeedb <ybzadough@gmail.com>
2018-07-05 20:04:55 -0400

    add main function

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
 const $ = document.querySelector.bind(document);
-const {
-  BehaviorSubject,
-  fromEvent,
-  interval,
-  merge,
-  range,
-  zip,
-  operators: {
-    filter,
-    map,
-    scan,
-    startWith,
-    tap
-  }
-} = rxjs;
-
 const sprite = $('#sprite');
 const meterContainer = $('#meter-container');
 const meter = $('#meter');
-
-const begin = fromEvent(document, 'keydown');
-const end = fromEvent(document, 'keyup');
 
 const powerLevels = {
   100: {
@@ -35,21 +16,31 @@ const powerLevels = {
   }
 };
 
-begin.pipe(
-  tap(() => {
-    sprite.classList.add('powerup');
-  }),
-  scan(acc => acc + 1, 0),
-  map(level => powerLevels[level]),
-  filter(level => level && level.next)
-)
-.subscribe(({ next, previous }) => {
-  console.log({ next, previous });
+const main = () => {
+  const { fromEvent } = rxjs;
+  const { filter, map, scan, tap } = rxjs.operators;
 
-  sprite.classList.remove(previous);
-  sprite.classList.add(next);
-});
+  const begin = fromEvent(document, 'keydown');
+  const end = fromEvent(document, 'keyup');
 
-end.subscribe(() => {
-  sprite.classList.remove('powerup');
-});
+  begin.pipe(
+    tap(() => {
+      sprite.classList.add('powerup');
+    }),
+    scan(acc => acc + 1, 0),
+    map(level => powerLevels[level]),
+    filter(level => level && level.next)
+  )
+  .subscribe(({ next, previous }) => {
+    console.log({ next, previous });
+
+    sprite.classList.remove(previous);
+    sprite.classList.add(next);
+  });
+
+  end.subscribe(() => {
+    sprite.classList.remove('powerup');
+  });
+};
+
+main();
